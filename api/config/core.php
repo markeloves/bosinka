48f6d5e7b35e2a7970bd7421930e55c3c9533329
EugeneSuz <gaster334@gmail.com>
2021-02-24 22:52:43 +0300

    fuck this shit

--- api/config/core.php
+++ api/config/core.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // URL домашней страницы
-$home_url="http://localhost/rest-api/api/";
+$home_url="http://95.131.149.21:30012/rest-api/api/";
 
 // страница указана в параметре URL, страница по умолчанию одна
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
